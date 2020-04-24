692e0699b86001088a4abdc41aaade3994cedf8b
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 13:39:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,13 +4,16 @@ def roll_call_dwarves(dwarves)
   end
 end
 
+
 def summon_captain_planet# code an argument here
   # Your code here
 end
 
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
